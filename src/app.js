2dfc6a1ed0ca24c01c1805af3d59e303c508c143
kerim <kerim.avci.611@gmail.com>
2021-10-24 12:44:47 +0300

    18 Ekim Ders Sonu 1. Ders

---
+++ src/app.js
@@ -0,0 +1,20 @@
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+
+for(let i = 0;i<konutKredileri.length;i++)
+{
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+
+ {/*
+    <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+    <ul> */
+}
+    
