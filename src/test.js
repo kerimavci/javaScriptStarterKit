22fb0aab955942bcd25a33ad2cd66c23e9c4f38a
kerim <kerim.avci.611@gmail.com>
2021-11-08 20:26:47 +0300

    Ödev1

--- src/test.js
+++ src/test.js
@@ -8,24 +8,29 @@ function isitPrime (...numbers) {
 
     for (let i = 0; i < numbers.length; i++) {
 
-        for (let j = 2; j < numbers[i]; j++ ) {
-
-            if (numbers[i] %j=== 0) {
-
-                console.log("Bu sayı asal değildir : " + numbers[i])
-
-                let prime = 0
-                prime = numbers[i]
-            }
+        if(numbers[i] == 0 || numbers[i] == 1 ) {
+            console.log("Bu sayı asal değildir : " + numbers[i])
+            continue
         }
-            if (prime != numbers[i]) 
 
-                {
-                console.log("Bu sayı asal sayıdır : " + numbers[i])
+            for (let j = 2; j < numbers[i]; j++ ) {
+
+                if (numbers[i] %j=== 0) {
+
+                    console.log("Bu sayı asal değildir : " + numbers[i])
+
+                    let common = 0
+                    common = numbers[i]
                 }
+            }
+                if (common != numbers[i]) 
+
+                    {
+                    console.log("Bu sayı asal sayıdır : " + numbers[i])
+                    }
     }
 }
-isitPrime(2,4,6,7,9,11,13,21)
+isitPrime(0,1,2,6,11,8,13,21)
 
 
 /* Parametre olarak girilen iki sayının arkadaş sayılar olup olmadığını bulan fonksiyon */
@@ -63,7 +68,7 @@ friendNumbers(220,284)
 
 /* 1000'e kadarki tüm mükemmel sayıları listeleyen fonksiyon */
 
-
+let perfectNumbers = function () {
 let toplam = 0
 console.log("1000'e kadar olan mükemmel sayılar sırasıyla şunlardır : ")
 
@@ -79,11 +84,38 @@ console.log("1000'e kadar olan mükemmel sayılar sırasıyla şunlardır : ")
             }
         }
 
-        if(i * 2 == toplam) {        
+                if(i * 2 == toplam) {        
 
-            console.log(i)       
+                    console.log(i)       
+                }
+    }
+}
+
+    
+    /* 1000'e kadar olan tüm asal sayıları listeleyen program */
+    
+let uptoThousand = function() {
+
+    let asal = 0
+    let k = 0
+
+    console.log("1000'e kadar olan asal sayılar şunlardır : ")
+
+        for (let a = 1; a < 1000; a++) {
+            
+            for (let b = 2; b < a; b++){
+
+                asal = a % b
+
+                    if(asal == 0) {
+                    break
+                    }
+            }
+            if(asal != 0){
+            k++
+            console.log(a)
+            }    
         }
 
-    
-    }
-    
+    console.log("Toplamda " + k + " adet asal sayı bulunur. ")
+}
